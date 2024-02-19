9f2ca1f87f8058549e01abd34434bcca5971f532
eddieling <eddielingg@gmail.com>
2024-02-20 07:58:07 +0800

    Remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Offer } from "./pages/Offer";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route exact path="/" component={Home} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/offer/:id" element={<Offer />} />
-        {/* <Route path="/about" component={About} /> */}
-        {/* <Route path="/contact" component={Contact} /> */}
       </Routes>
     </BrowserRouter>
   );
