d4483558c3fc653540df423e414b320fc5741df3
eddieling <eddielingg@gmail.com>
2024-02-20 09:28:11 +0800

    Debug offer page

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import styles from "../styles/Offer.module.css";
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import styles from "../styles/Offer.module.css";
 
 export const Offer = () => {
   // const { id } = useParams();
